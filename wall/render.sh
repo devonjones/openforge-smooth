6aea9153ae295c9e65268bef79d8ebababdfb710
Devon Jones <devon.jones@gmail.com>
2015-03-14 17:17:41 -0600

    fixed wall rendering

--- wall/render.sh
+++ wall/render.sh
@@ -6,7 +6,7 @@ for x in {1..4}
 do
   for y in {1..4}
   do
-    $OPENSCAD -o smooth_wall_1x1.stl \
+    $OPENSCAD -o smooth_wall_${x}x${y}.stl \
       -D 'render=true' -D "x=$x" -D "y=$y" \
       smooth_wall.scad
   done
