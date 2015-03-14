374a84b02e808b72494eb38cb1ac46c605614c5f
Devon Jones <devon.jones@gmail.com>
2015-03-14 16:58:15 -0600

    fixed rendering for dead end

--- dead_end/render.sh
+++ dead_end/render.sh
@@ -6,7 +6,7 @@ for x in {2..4}
 do
   for y in {1..4}
   do
-    $OPENSCAD -o smooth_dead_end_2x1.stl \
+    $OPENSCAD -o smooth_dead_end_${x}x${y}.stl \
       -D 'render=true' -D "x=$x" -D "y=$y" \
       smooth_dead_end.scad
   done
