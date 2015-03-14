db0b74c7f0552d4a8ddc0372191093175b34d0b3
Devon Jones <devon.jones@gmail.com>
2015-03-14 16:14:41 -0600

    cleaned up render.sh

--- corridor/render.sh
+++ corridor/render.sh
@@ -2,50 +2,12 @@
 
 : ${OPENSCAD:="openscad"}
 
-$OPENSCAD -o smooth_corridor_1x2.stl \
-  -D 'render=true' -D 'x=1' -D 'y=2' \
-  smooth_corridor.scad
-
-$OPENSCAD -o smooth_corridor_1x3.stl \
-  -D 'render=true' -D 'x=1' -D 'y=3' \
-  smooth_corridor.scad
-
-$OPENSCAD -o smooth_corridor_1x4.stl \
-  -D 'render=true' -D 'x=1' -D 'y=4' \
-  smooth_corridor.scad
-
-$OPENSCAD -o smooth_corridor_2x2.stl \
-  -D 'render=true' -D 'x=2' -D 'y=2' \
-  smooth_corridor.scad
-
-$OPENSCAD -o smooth_corridor_2x3.stl \
-  -D 'render=true' -D 'x=2' -D 'y=3' \
-  smooth_corridor.scad
-
-$OPENSCAD -o smooth_corridor_2x4.stl \
-  -D 'render=true' -D 'x=2' -D 'y=4' \
-  smooth_corridor.scad
-
-$OPENSCAD -o smooth_corridor_3x2.stl \
-  -D 'render=true' -D 'x=3' -D 'y=2' \
-  smooth_corridor.scad
-
-$OPENSCAD -o smooth_corridor_3x3.stl \
-  -D 'render=true' -D 'x=3' -D 'y=3' \
-  smooth_corridor.scad
-
-$OPENSCAD -o smooth_corridor_3x4.stl \
-  -D 'render=true' -D 'x=3' -D 'y=4' \
-  smooth_corridor.scad
-
-$OPENSCAD -o smooth_corridor_4x2.stl \
-  -D 'render=true' -D 'x=4' -D 'y=2' \
-  smooth_corridor.scad
-
-$OPENSCAD -o smooth_corridor_4x3.stl \
-  -D 'render=true' -D 'x=4' -D 'y=3' \
-  smooth_corridor.scad
-
-$OPENSCAD -o smooth_corridor_4x4.stl \
-  -D 'render=true' -D 'x=4' -D 'y=4' \
-  smooth_corridor.scad
+for x in {1..4}
+do
+  for y in {2..4}
+  do
+    $OPENSCAD -o smooth_corridor_2x1.stl \
+      -D 'render=true' -D "x=$x" -D "y=$y" \
+      smooth_corridor.scad
+  done
+done
--- diagonal/render.sh
+++ diagonal/render.sh
@@ -2,35 +2,14 @@
 
 : ${OPENSCAD:="openscad"}
 
-$OPENSCAD -o smooth_diagonal_1x1.stl \
-  -D 'render_diagonal=true' -D 'x=1' \
-  smooth_diagonal.scad
+for x in {1..4}
+do
+  $OPENSCAD -o smooth_diagonal_1x1.stl \
+    -D 'render_diagonal=true' -D "x=$x" \
+    smooth_diagonal.scad
 
-$OPENSCAD -o smooth_diagonal_2x2.stl \
-  -D 'render_diagonal=true' -D 'x=2' \
-  smooth_diagonal.scad
+  $OPENSCAD -o smooth_diagonal_half_floor_1x1.stl \
+    -D 'render_diagonal_half_floor=true' -D "x=$x" \
+    smooth_diagonal.scad
+done
 
-$OPENSCAD -o smooth_diagonal_3x3.stl \
-  -D 'render_diagonal=true' -D 'x=3' \
-  smooth_diagonal.scad
-
-$OPENSCAD -o smooth_diagonal_4x4.stl \
-  -D 'render_diagonal=true' -D 'x=4' \
-  smooth_diagonal.scad
-
-
-$OPENSCAD -o smooth_diagonal_half_floor_1x1.stl \
-  -D 'render_diagonal_half_floor=true' -D 'x=1' \
-  smooth_diagonal.scad
-
-$OPENSCAD -o smooth_diagonal_half_floor_2x2.stl \
-  -D 'render_diagonal_half_floor=true' -D 'x=2' \
-  smooth_diagonal.scad
-
-$OPENSCAD -o smooth_diagonal_half_floor_3x3.stl \
-  -D 'render_diagonal_half_floor=true' -D 'x=3' \
-  smooth_diagonal.scad
-
-$OPENSCAD -o smooth_diagonal_half_floor_4x4.stl \
-  -D 'render_diagonal_half_floor=true' -D 'x=4' \
-  smooth_diagonal.scad
