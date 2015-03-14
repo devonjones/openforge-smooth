11f298ab2ba84b51b8f39f6f0f3a59dbcb393912
Devon Jones <devon.jones@gmail.com>
2015-03-14 16:41:59 -0600

    fixing corridor rendering

--- corridor/render.sh
+++ corridor/render.sh
@@ -6,7 +6,7 @@ for x in {1..4}
 do
   for y in {2..4}
   do
-    $OPENSCAD -o smooth_corridor_2x1.stl \
+    $OPENSCAD -o smooth_corridor_${x}x${y}.stl \
       -D 'render=true' -D "x=$x" -D "y=$y" \
       smooth_corridor.scad
   done
